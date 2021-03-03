ac347659f2e04a6333aac5bfb9f12b74b950e2b7
Ben Armston <ben.armston@alces-software.com>
2021-03-03 11:34:22 +0000

    Add super simple test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+async function renderApp() {
+  const utils = render(<App />);
+  expect(utils.getByText('Loading...')).toBeInTheDocument();
+  await waitFor(
+    () => expect(utils.queryByText('Loading...')).toBeNull()
+  );
+  return utils;
+}
+
+test('renders without crashing', async () => {
+  const { queryByText } = await renderApp();
+  expect(queryByText('An error has occurred')).toBeNull();
 });
