cc44e0c40dabb88f323328a77df54de4236196a3
Ben Armston <ben.armston@alces-software.com>
2021-03-11 14:54:52 +0000

    Rework location of environment name

--- src/UnauthenticatedDashboard.js
+++ src/UnauthenticatedDashboard.js
@@ -8,6 +8,7 @@ function UnauthenticatedDashboard() {
   return (
     <div>
       <DashboardLogo />
+      <ClusterOverview className="mt-2 mb-2" />
       <p>
         Flight Console Service allows you to access an interactive terminal
         console session running on your cluster from the comfort of your
@@ -18,10 +19,6 @@ function UnauthenticatedDashboard() {
         To start interacting with your console and gain access to your HPC
         environment sign in above.
       </p>
-
-      <div className="card-deck">
-        <ClusterOverview />
-      </div>
     </div>
   );
 }
