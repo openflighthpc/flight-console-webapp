81cfd26862cefd8f0160ba0ef6163c0a8852ba07
Ben Armston <ben.armston@alces-software.com>
2021-10-25 15:53:59 +0100

    Fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, waitFor } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import App from './App';
 
 async function renderApp() {
@@ -14,4 +14,29 @@ async function renderApp() {
 test('renders without crashing', async () => {
   const { queryByText } = await renderApp();
   expect(queryByText('An error has occurred')).toBeNull();
+});
+
+test('can sign in', async () => {
+  const {
+    getByText, getByRole, getByLabelText, queryByText,
+  } = await renderApp();
+
+  expect(queryByText(/test-user/)).toBeNull();
+  expect(queryByText(/Test user/)).toBeNull();
+
+  const loginButton = getByText(/Log in/);
+  expect(loginButton).toBeInTheDocument();
+  fireEvent.click(loginButton)
+
+  const nameInput = getByLabelText('Enter your username');
+  const passwordInput = getByLabelText('Enter your password');
+  const button = getByRole('button', { name: 'Sign in' });
+  fireEvent.change(nameInput, { target: { value: 'test-user' } });
+  fireEvent.change(passwordInput, { target: { value: 'test-password' } });
+  fireEvent.click(button);
+  await waitFor(
+    () => expect(queryByText(/Log in/)).toBeNull()
+  )
+  expect(getByText('test-user')).toBeInTheDocument();
+  expect(getByText('Test user')).toBeInTheDocument();
 });
